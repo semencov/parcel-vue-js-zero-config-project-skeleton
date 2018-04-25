e4df75fb91ca9e2331b77a76043ed9d8389b2712
Simon Dann <carbontwelve@users.noreply.github.com>
2018-04-25 22:16:17 +0100

    :tada: initial commit of tutorial files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Hurray! ParcelJS Zero Config Vue app is running!'
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+    #app {
+        color: red;
+    }
+</style>
